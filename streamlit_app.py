55dbe41be1007cbde5725d04b8bdcc7fa90f440e
SaurabhTholiya <104692528+SaurabhTholiya@users.noreply.github.com>
2023-07-07 06:45:49 +0530

    Update streamlit_app.py
    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 streamlit.title('herbs')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 🥗 🐔 🥑🍞
+Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
